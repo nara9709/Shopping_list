d537f15b655e897f058745e72153a1f835b5b573
Nara lee <worldkr19@gmail.com>
2022-02-03 02:51:11 -0500

    JS

---
+++ main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const addBtn = document.querySelector('.add__btn');
+const items = document.querySelector('.items');
+const deleteBtn = document.querySelector('.delete__btn');
+
+function onAddBtn() {
+  //1. get a value of text input
+  //2. add list when input btn is clicked
+  //3. reset
+}
