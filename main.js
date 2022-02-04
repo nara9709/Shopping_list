2df1bd0b6ceb5fc78df05d75229a7e018f2c7d22
Nara lee <worldkr19@gmail.com>
2022-02-04 02:14:30 -0500

    JS

--- main.js
+++ main.js
@@ -1,11 +1,67 @@
 'use strict';
 
 const addBtn = document.querySelector('.add__btn');
+const listBox = document.querySelector('.list');
 const items = document.querySelector('.items');
 const deleteBtn = document.querySelector('.delete__btn');
+const textInput = document.querySelector('.text__input');
 
 function onAddBtn() {
-  //1. get a value of text input
-  //2. add list when input btn is clicked
-  //3. reset
+  let name = textInput.value;
+  if (name === '') {
+    textInput.focus();
+    return;
+  } else {
+    createListItem(name);
+
+    textInput.value = '';
+    textInput.focus();
+  }
 }
+
+function createListItem(name) {
+  let createList = document.createElement('li');
+  createList.setAttribute('class', 'item');
+
+  let createitemRow = document.createElement('div');
+  createitemRow.setAttribute('class', 'item__row');
+
+  let createItemName = document.createElement('span');
+  createItemName.setAttribute('class', 'item__name');
+
+  let createDelete = document.createElement('button');
+  createDelete.setAttribute('class', 'delete__btn');
+
+  let createDelIcon = document.createElement('i');
+  createDelIcon.setAttribute('class', 'fas fa-trash-alt');
+
+  let createDivider = document.createElement('div');
+  createDivider.setAttribute('class', 'divider');
+
+  items.appendChild(createList);
+  items.appendChild(createDivider);
+  createList.appendChild(createitemRow);
+  createitemRow.appendChild(createItemName);
+  createitemRow.appendChild(createDelete);
+  createDelete.appendChild(createDelIcon);
+
+  createItemName.innerHTML = name;
+
+  createList.scrollIntoView({ black: 'center' });
+
+  createDelete.addEventListener('click', () => {
+    items.removeChild(createList);
+    items.removeChild(createDivider);
+  });
+}
+
+addBtn.addEventListener('click', () => {
+  onAddBtn();
+});
+
+//add with enter key
+document.addEventListener('keypress', (event) => {
+  if (event.key === 'Enter') {
+    onAddBtn();
+  }
+});
